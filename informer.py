4f98cd07bf2cfecc811e701838fce4e422fdfe67
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-02-20 21:32:20 +0300

    a lot of changes

--- informer.py
+++ informer.py
@@ -2,31 +2,31 @@ class Informer:
     def __init__(self):
         pass
 
-    def set_loading(self, text="loading..."):
+    def set_loading(self, text: str = "loading..."):
         print(f"[...] {text}...{' ' * 10}", end='\r')
 
-    def set_info(self, ret: bool, text="info"):
+    def set_info(self, ret: bool, text: str = "info"):
         if ret:
             print(f"[i] {text}" + ' ' * 10, end='\r')
         else:
             print(f"[i] {text}" + ' ' * 10, end='\n')
 
-    def set_question(self, ret_type: type = str, text="question"):
+    def set_question(self, ret_type: type = str, text: str = "question"):
         return ret_type(input(f"[?] {text}: "))
 
-    def set_success(self, ret: bool, text="success!"):
+    def set_success(self, ret: bool, text: str = "success!"):
         if ret:
             print(f"[+] {text}" + ' ' * 10, end='\r')
         else:
             print(f"[+] {text}" + ' ' * 10, end='\n')
 
-    def set_warning(self, ret: bool, text="warning!"):
+    def set_warning(self, ret: bool, text: str = "warning!"):
         if ret:
             print(f"[!] {text}" + ' ' * 10, end='\r')
         else:
             print(f"[!] {text}" + ' ' * 10, end='\n')
 
-    def set_error(self, ret: bool, text="error!"):
+    def set_error(self, ret: bool, text: str = "error!"):
         if ret:
             print(f"[×] {text}" + ' ' * 10, end='\r')
         else:
--- main.py
+++ main.py
@@ -1,77 +1,34 @@
-try:
-    from informer import Informer
+from informer import Informer
+from astropy.coordinates import EarthLocation, solar_system_ephemeris
+import time
+from get_loc import get_loc
+from ephems import solar_system_full, custom_bodies
+from update_time import update_time
 
-    inf = Informer()
-    inf.set_loading('initing')
+inf = Informer()
+inf.set_loading('initializing')
 
-    from astropy.time import Time
-    from astropy.coordinates import solar_system_ephemeris, EarthLocation, SkyCoord, AltAz
-    from astropy.coordinates import get_body, get_moon, get_sun
-    import time
-    from prettytable import PrettyTable
-    from convert_coord import convert_coord
-    from get_loc import get_loc
-    from ephems import solar_system_full, custom_bodies
 
-    def update_time():
-        global time_now, time_str
-        while True:
-            print('''\nTime entry options:    
-    now. Getting time set in system
-    custom. Нou must manually enter the time''')
-            time_mode = input('you choice: ')
-            if time_mode == 'now':
-                time_str = time.strftime('%Y-%m-%d %H:%M:%S')
-                time_now = Time(time_str, format='iso')
-                break
-            elif time_mode == 'custom':
-                inf.set_warning(False, 'please enter UTC time!')
-                inf.set_info(False, 'UTC = Tп - GMT')
-                try:
-                    values = [
-                        int(input(comp + ': '))
-                        for comp in ['hour', 'min', 'sec', 'day', 'month', 'year']
-                    ]
-                    if values[-1] not in range(1900, 2101):
-                        inf.set_warning(False, 'year must be in range 1900-2100 AD')
-                        raise ValueError
-                except ValueError:
-                    inf.set_error(False, 'invalid data')
-                    continue
-                time_str = f'{values[5]}-{values[4]}-{values[3]} {values[0]}:{values[1]}:{values[2]}'
-                try:
-                    time_now = Time(time_str, format='iso')
-                except ValueError:
-                    inf.set_error(False, f'invalid date: {time_str}')
-                    continue
-                break
-            else:
-                inf.set_error(False, 'invalid choice')
-                print()
-                continue
-        inf.set_success(False, f'time has been set: {time_str}')
-        return (time_now, time_str)
+inf.set_success(True, 'ready to work')
+time.sleep(0.5)
 
-    inf.set_success(True, 'ready to work')
-    time.sleep(0.5)
+loc: EarthLocation = get_loc()
 
-    loc = get_loc()
+solar_system_ephemeris.bodies = list(solar_system_ephemeris.bodies)
+solar_system_ephemeris.bodies.remove('earth')
+solar_system_ephemeris.bodies.remove('earth-moon-barycenter')
 
-    table = PrettyTable()
-    table.field_names = [
-        '№', 'Name of body', 'Right ascension, hms', 'Declination, dms',
-        'Azimuth, °', 'Altitude, °', 'Distance'
-    ]
+print(
+	"available bodies to compute: 'all', 'sun', 'moon', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', "
+	"'neptune'")
 
-    solar_system_ephemeris.bodies = list(solar_system_ephemeris.bodies)
-    solar_system_ephemeris.bodies.remove('earth')
-    solar_system_ephemeris.bodies.remove('earth-moon-barycenter')
+bodies_to_compute: tuple[str] = tuple(input("[?] enter bodies for calculation, separating their by space: ").lower().split())
 
-    #update_time()
+time_now, _ = update_time()
 
-    #solar_system_full(time_now=time_now, loc=loc, table=table)
-    custom_bodies(upd_time=update_time, loc=loc, table=table)
-    
-except KeyboardInterrupt:
-    print('\n')
-    inf.set_warning(False, 'program interrupted by user')
+if bodies_to_compute == ['all']:
+	solar_system_full(time_now, loc)
+elif 'all' in bodies_to_compute:
+	raise ValueError()
+else:
+	custom_bodies(time_now, loc, bodies_to_compute)
